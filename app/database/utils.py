d0a40e3aeee668d04897e3bac0c8ae50a8351f45
Lautaro Coo <lautarocoo@gmail.com>
2022-12-08 18:39:43 -0300

    changes to users logic in routers, utils for logic and raising errors

--- app/database/utils.py
+++ app/database/utils.py
@@ -1,26 +1,130 @@
+from ..database import models, queries 
+from ..utils import schemas
 from .database import Base
+from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 from passlib.context import CryptContext
+from typing import List, Optional
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def raise_404_if_register_not_exist(register: Base, id: int, name_String: str):
-    if not register:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,  detail=f"{name_String.upper()} with ID: {id} does not exist")
-    return
-
-
-def raise_403_if_user_not_authorized(register, current_user_id):
-    if register.created_by_user_id != current_user_id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,  detail="Not Authorized to perform requested action")
-    return
-
-
-def hash(password: str) -> str:
+def hash(
+    password: str
+    ) -> str:
     return pwd_context.hash(password)
 
 
-def verify(plain_password, hashed_password):
+def verify(
+    plain_password: str, 
+    hashed_password: str
+    ) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
+
+def is_user_admin(
+    db: Session, 
+    current_user_id: int,
+    hardcoded_user_id: int = 1, 
+    hardcoded_group_id: int = 1
+    ) -> bool:
+
+    if current_user_id == hardcoded_user_id:
+        return True
+
+    users_data = db.query(models.User.id).filter(models.User.group_id==hardcoded_group_id)
+    users_allowed = [id[0] for id in users_data] 
+    
+    if current_user_id in users_allowed:
+        return True
+    
+    return False
+
+
+def is_user_status_locked(
+    db: Session, 
+    current_user_id: int, 
+    user_affected_id: int, 
+    hardcoded_user_id: int = 1,
+    hardcoded_group_id: List[int] = [1]
+    ) -> bool:
+    
+    if user_affected_id == hardcoded_user_id:
+        return True
+    
+    if user_affected_id == current_user_id:
+        return True
+
+    users_data = db.query(models.User.id).filter(models.User.group_id.in_(hardcoded_group_id)).all()
+    users_id_in_locked_groups: List[int] = [id[0] for id in users_data]
+
+    if user_affected_id in users_id_in_locked_groups:
+        return True
+
+    return False
+
+
+def raise_404_if_register_not_exist(
+    register: Base,
+    id: int,
+    name_String: str
+    ) -> None:
+    if not register:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,  detail=f"{name_String.upper()} with ID: {id} does not exist")
+    return None
+
+
+def raise_403_if_user_not_authorized(
+    register: Base, 
+    current_user_id: int
+    ) -> None:
+    if register.created_by_user_id != current_user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,  detail="Not Authorized to perform requested action")
+    return None
+
+
+def raise_403_if_user_is_not_admin(
+    db: Session, 
+    current_user: models.User, 
+    action_string: Optional[str] = None
+    ) -> None:
+
+    if action_string is not None:
+        action_string = " - " + action_string
+
+    if not is_user_admin(db,current_user.id):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"Not Authorized to perform requested action{action_string}"
+        )
+
+    return None
+
+
+def raise_403_if_user_must_stay_Active(
+    db: Session, 
+    current_user: models.User, 
+    user_affected: models.User
+    ) -> None:
+
+    if is_user_status_locked(db,current_user.id,user_affected.id):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Unable to perform requested action - User must remain active"
+        )
+    
+    return None
+
+
+def _change_user_state(db,updated_user_fields: schemas.UserChangeState, affected_user: models.User):
+    if updated_user_fields.active == affected_user.active:
+        return affected_user
+    if updated_user_fields.active is None:
+        updated_user_fields.active = not affected_user.active
+
+    user_register = db.query(models.User).filter(models.User.id == affected_user.id).first()
+    user_register.active = updated_user_fields.active
+    db.commit()
+    
+    return user_register
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,9 +1,11 @@
 from fastapi import status, Depends, APIRouter, HTTPException
+from sqlalchemy import and_, or_, not_
 from sqlalchemy.orm import Session
 from typing import List
 from ..database import models, queries, utils, oauth2
 from ..database.database import get_db
 from ..utils import schemas
+
 
 
 router = APIRouter(
@@ -56,20 +58,22 @@ async def get_user(id: int, db: Session = Depends(get_db), current_user: models.
 @router.put("/state/{id}", response_model=schemas.UserResponse)
 async def change_user_state(id: int, updated_user_fields: schemas.UserChangeState, db: Session = Depends(get_db), current_user: models.User = Depends(oauth2.get_current_user)):
     
+    utils.raise_403_if_user_is_not_admin(db,current_user,action_string='Change User State')
 
-    if current_user.id != 1 or id ==1:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Not Authorized to perform requested action - Change User State"
-        )
     user = queries.get_register_by_id(models.User, db, id)
     utils.raise_404_if_register_not_exist(user, id, 'User')
-    if updated_user_fields.active is None:
-        updated_user_fields.active = not user.active
-    if updated_user_fields.active == user.active:
-        return user
-    user_register = queries.__get_register_by_id(models.User, db, id)
-    return queries.update_register(user_register, db, updated_user_fields)
+
+    utils.raise_403_if_user_must_stay_Active(db,current_user,user)
+
+    # if updated_user_fields.active == user.active:
+    #     return user
+    # if updated_user_fields.active is None:
+    #     updated_user_fields.active = not user.active
+
+    # user_register = queries.__get_register_by_id(models.User, db, id)
+    # return queries.update_register(user_register, db, updated_user_fields)
+
+    return utils._change_user_state(db,updated_user_fields,user)
 
 
 @router.put("/{id}", response_model=schemas.UserResponse)
